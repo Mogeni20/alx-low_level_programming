7598e9e8549f45b62295fb63ed2124fa504b4422
Mogeni20 <mogeniyvonne20@gmail.com>
2023-09-05 16:40:55 -0700

    1-create_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,21 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd, x, y;
+	char *buf;
+	ssize_t fd, x, y;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    y = read(fd, buf, letters);
-    x = write(STDOUT_FILENO, buf, y);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+	close(fd);
+	return (0);
+	}
 
-    free(buf);
-    close(fd);
+	y = read(fd, buf, letters);
+	x = write(STDOUT_FILENO, buf, y);
 
-    return (x);
+	free(buf);
+	close(fd);
+
+	return (x);
 }
